f312f0adbbf959444b2ec682bd20569136210e94
alanalendron <78566149+alanalendron@users.noreply.github.com>
2021-08-22 02:38:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleEntrar(){
+  var confirmPassword=document.getElementById("confirmPassword")
+  confirmPassword.style.display="none"
+}
+
+function toggleRegistrar(){
+  var confirmPassword=document.getElementById("confirmPassword")
+  confirmPassword.style.display="block"
+}
